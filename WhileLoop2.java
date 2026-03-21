5c3a51609db1e50be5e96db3be99a99bb4f64e5e
Saurabh Rajbhar <saurabhrajbhar100@gmail.com>
2026-03-21 17:12:10 +0530

    Add files via upload

---
+++ WhileLoop2.java
@@ -0,0 +1,11 @@
+public class WhileLoop2 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 10) {
+            System.out.println("Saurabh");
+            i=i+2;
+        }
+    }
+}
+
