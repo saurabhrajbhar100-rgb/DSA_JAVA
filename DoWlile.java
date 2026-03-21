89bab7ede3a26487059539806a8fb8101636a078
Saurabh Rajbhar <saurabhrajbhar100@gmail.com>
2026-03-21 18:00:47 +0530

    Add files via upload

---
+++ DoWlile.java
@@ -0,0 +1,19 @@
+//do{
+//}while (codition cheak);
+
+
+public class DoWlile {
+    
+public static void main(String []args){
+
+int i=0;
+do{
+
+    System.out.println("GFG");
+    i++;
+}
+while (i<10);
+}
+
+    }
+
