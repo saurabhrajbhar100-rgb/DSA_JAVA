d4afe069636b81504ee2ef789d13aef2411ce76d
Saurabh Rajbhar <saurabhrajbhar100@gmail.com>
2026-03-21 18:09:55 +0530

    Add files via upload

---
+++ DoWile2.java
@@ -0,0 +1,13 @@
+public class DoWile2 {
+    public static void main(String [] arge){
+
+        int i =20;
+        do { 
+            
+            System.out.println("GFG");
+            i++;
+
+        }
+         while(i<10);
+    }
+}
