1e8069210d3b7abb77b64a09a6b2541a39190d3f
Saurabh Rajbhar <saurabhrajbhar100@gmail.com>
2026-05-06 12:28:29 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,16 @@
+public class Function {
+
+    public static void main(String[] args) {
+
+        System.out.println("Before Call");
+        fun();
+
+        System.out.println("After Call");
+
+    }
+
+    public static void fun() {
+        System.out.println("Inside Fun");
+    }
+
+}
