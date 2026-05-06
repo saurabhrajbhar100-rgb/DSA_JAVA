ea0dba5d7bd1528c3134add9cd37833b80651844
Saurabh Rajbhar <saurabhrajbhar100@gmail.com>
2026-05-06 13:10:17 +0530

    Add files via upload

---
+++ Method2.java
@@ -0,0 +1,16 @@
+public class Method2 {
+    
+    public static void main(String[] agrs){
+
+        fun();
+        fun();
+
+    }
+    public static void fun(){
+        
+        int x=5,y=10;
+
+        System.out.println(x+y);
+        
+    }
+}
